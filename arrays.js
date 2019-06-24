5c07455243f348bf11d0e41d78795c138a332140
Marcal Ramos <suncastelo@gmail.com>
2019-06-24 05:11:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,26 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 destructivelyAddElementToEndOfArray();
+
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
